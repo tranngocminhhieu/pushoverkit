83f573b594fec57d28220fa9f1321b7ab29e5b63
Tran Ngoc Minh Hieu <tnmhieu@gmail.com>
2025-06-10 22:43:28 +0700

    Test

--- tests/test_message.py
+++ tests/test_message.py
@@ -2,4 +2,4 @@ from config import token, user
 from pushoverkit import Pushover
 
 pushover = Pushover(token=token, default_user=user)
-print(pushover.push(message='Hello World! Test from <a href="https://github.com/tranngocminhhieu/pushoverkit">PushoverKit</a>', title='Test title', monospace=1))
+print(pushover.push(message='Hello World! Test from <a href="https://github.com/tranngocminhhieu/pushoverkit">PushoverKit</a>', title='Test title', html=1, attachment='image.png'))
